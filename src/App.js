8aa77952bb635384d9c9f3d4ff2e20bd484a8b70
Maria <bc-macair023@BC-macair023s-MacBook-Air.local>
2021-11-15 11:51:21 +0200

    add all my files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Speedgame</h1>
+        <p> Your score:</p>
+        <button>Start</button>
+        <button>Stop</button>
+      </div>
+    );
+  }
+}
+
+export default App;
